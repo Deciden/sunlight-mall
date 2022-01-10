abdddf50bff8faab50d05d8d3335232d19ec48fb
Deciden <90779024+Deciden@users.noreply.github.com>
2022-01-10 13:13:40 +0100

    Delete Sunlight Mall HTML directory

--- Sunlight Mall HTML/js/burgermenu.js
+++
@@ -1,7 +0,0 @@
-  function toggleNavigation(x,target) {
-    var targetElement= document.getElementById(target);
-    console.log(target);
-    targetElement.classList.toggle("change");
-
-    x.classList.toggle("change");
-  } 
